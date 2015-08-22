5ff9ae96ede4b65ed6dce1ca0cc8867b654b1b62
Eric Koslow <ekoslow@gmail.com>
2015-08-22 16:47:37 -0700

    Extract react server rendering code

--- server/index.js
+++ server/index.js
@@ -1,52 +1,7 @@
 import express from 'express';
-import React from 'react';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router';
-import Location from 'react-router/lib/Location';
-import routes from '../shared/routes';
-import * as reducers from '../shared/reducers';
-
+import reactServer from './react-server';
 const app = express();
 
-app.use((req, res) => {
-  const location = new Location(req.path, req.query);
-  const reducer = combineReducers(reducers);
-  const store = createStore(reducer);
-
-  Router.run(routes, location, (err, routeState) => {
-    if (err) return console.error(err);
-    if (!routeState) return res.status(404).end('404');
-
-    const InitialComponent = (
-      <Provider store={store}>
-        {() =>
-          <Router {...routeState} />
-        }
-      </Provider>
-    );
-    const componentHTML = React.renderToString(InitialComponent);
-    const initialState = store.getState();
-
-    const HTML = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Postcards</title>
-      </head>
-      <body>
-        <div id="react-view">${componentHTML}</div>
-        <script>
-          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-        </script>
-        <script type="application/javascript" src="/app.js"></script>
-      </body>
-    </html>
-    `;
-
-    res.end(HTML);
-  });
-});
+app.use(reactServer);
 
 export default app;
