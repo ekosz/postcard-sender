993c045ce2391e81833c69de7b767e49e67f9a8f
Eric Koslow <ekoslow@gmail.com>
2015-08-22 16:04:32 -0700

    Move postcards to its own component

--- shared/components/index.js
+++ shared/components/index.js
@@ -1,35 +1,10 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { retrievePath } from '../actions/falcor';
+import Postcards from './postcards';
 
 class Root extends Component {
-  componentDidMount() {
-    const { retrievePath } = this.props;
-
-    retrievePath(['postcards', 0, 'name']);
-  }
-
   render() {
-    const { postcards } = this.props;
-
-    let output;
-
-    if (!postcards) {
-      output = <h1>Loading...</h1>;
-    } else if (!postcards.get("0")) {
-      output = <h1>No Postcards created yet</h1>;
-    } else {
-      output = <ul>{postcards.map(card => { return <li>{card.get("name")}</li> })}</ul>;
-    }
-
-    return output;
+    return <Postcards />;
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    postcards: state.entities.get('postcards')
-  };
-}
-
-export default connect(mapStateToProps, { retrievePath })(Root);
+export default Root;
---
+++ shared/components/postcards.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { retrievePath } from '../actions/falcor';
+
+class Postcards extends Component {
+  componentDidMount() {
+    const { retrievePath } = this.props;
+
+    retrievePath(['postcards', 0, 'name']);
+  }
+
+  render() {
+    const { postcards } = this.props;
+
+    let output;
+
+    if (!postcards) {
+      output = <h1>Loading...</h1>;
+    } else if (!postcards.get("0")) {
+      output = <h1>No Postcards created yet</h1>;
+    } else {
+      output = <ul>{postcards.map(card => { return <li>{card.get("name")}</li> })}</ul>;
+    }
+
+    return output;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    postcards: state.entities.get('postcards')
+  };
+}
+
+export default connect(mapStateToProps, { retrievePath })(Postcards);
